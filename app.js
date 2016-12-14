3b77f1bde91f65c5954c7c2573c4cdb30fdc184a
fix <fx.thoorens@gmail.com>
2016-12-14 11:38:01 +0100

    improve overall UX

--- app.js
+++ app.js
@@ -1,44 +1,63 @@
 var bip39 = require("bip39");
 var ark = require("arkjs");
-var request = require('request');
 
 var testnetapi = "http://node1.arknet.cloud:4000/api/"
 
 var testnetrequest = function(api, callback){
-  request(testnetapi + api, function (error, response, body) {
-    callback(error, JSON.parse(body));
+  $.getJSON(testnetapi + api, function(data) {
+    callback(data);
   });
 };
 
-
 $(window).ready(function(){
-
-  $("button#generateAccountc").click(function(){
+  $("button#generateAccount").click(function(){
     var passphrase = bip39.generateMnemonic();
+    var address = ark.crypto.getAddress(ark.crypto.getKeys(passphrase).publicKey)
     $("#passphrase").html(passphrase);
-    $("#address").html(ark.crypto.getAddress(ark.crypto.getKeys(passphrase).publicKey));
+    $("#address").html(address);
+    $('#qrcode').html("");
+    $('#qrcode').qrcode("{'a':'"+address+"'}");
   });
 
-  $("button#queryTestnet").click(function(){
+  $("form#queryTestnet").submit(function(e){
+    e.preventDefault();
     testnetapi = $("#testnet_node").val() + "/api/";
-    testnetrequest("loader/status/sync", function(error, result){
+    testnetrequest("loader/status/sync", function(result){
       console.log(result);
       $("#height").html(result.height);
     });
   });
 
-  $("button#checkTransactions").click(function(){
+  $("form#checkTransactions").submit(function(e){
+    e.preventDefault();
     var address = $("#check_address").val();
     console.log(address);
-    testnetrequest("transactions?orderBy=timestamp:desc&recipientId="+address+"&senderId="+address, function(error, result){
+    testnetrequest("transactions?orderBy=timestamp:desc&recipientId="+address+"&senderId="+address, function(result){
       console.log(result);
       $("#transactions").html("");
       for(i in result.transactions){
         var transaction=result.transactions[i];
+        if(transaction.type==0){
+          transaction.type="Send";
+        }
+        else if(transaction.type==1){
+          transaction.type="Delegate Registration";
+        }
+        else if(transaction.type==2){
+          transaction.type="Second Passphrase Creation";
+        }
+        else if(transaction.type==3){
+          transaction.type="Vote";
+        }
+        else if(transaction.type==4){
+          transaction.type="Multisignature";
+        }
         var tr = $("<tr>")
+        tr.append("<td><a target='_blank' href='http://texplorer.ark.io/tx/"+transaction.id+"'>"+transaction.id+"</a></td>");
         tr.append("<td>"+transaction.type+"</td>");
-        tr.append("<td>"+transaction.id+"</td>");
-        tr.append("<td>"+transaction.vendorField+"</td>");
+        tr.append("<td><a target='_blank' href='http://texplorer.ark.io/address/"+transaction.senderId+"'>"+transaction.senderId+"</a></td>");
+        tr.append("<td><a target='_blank' href='http://texplorer.ark.io/address/"+transaction.recipientId+"'>"+transaction.recipientId+"</a></td>");
+        tr.append("<td>"+(transaction.vendorField?transaction.vendorField:"-")+"</td>");
         tr.append("<td>"+transaction.amount/100000000+"</td>");
         tr.append("<td>"+transaction.fee/100000000+"</td>");
         $("#transactions").append(tr);
